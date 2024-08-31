3c1f2b6854c8de624f398bcb3361fac8e179926b
Zaggy1024 <Zaggy1024@gmail.com>
2024-08-31 06:55:17 -0500

    Find and destroy handheld cameras via FindObjectsByType()
    Previously, we were using RoundManager.Instance.spawnSyncedObjects, which is only populated on the host where the synced objects are spawned. By getting them with FindObjectsByType(), the clients can find these network-spawned objects and spawn the necessary explosions on their end.

--- BlackMesa/PatchStartOfRound.cs
+++ BlackMesa/PatchStartOfRound.cs
@@ -10,17 +10,9 @@ namespace BlackMesa
         [HarmonyPatch(nameof(StartOfRound.ShipHasLeft))]
         private static void ShipHasLeftPostFix()
         {
-            Debug.Log("Trying to DespawnProps");
-            foreach (var spawnedSyncedObject in RoundManager.Instance.spawnedSyncedObjects)
-            {
-                Debug.Log($"{spawnedSyncedObject}");
-                if (spawnedSyncedObject.TryGetComponent<HandheldTVCamera>(out var handheldTV))
-                {
-                    handheldTV.DestroyTv();
-                    //Object.Destroy(spawnedSyncedObject);
-                    //Debug.Log("Trying to delete it");
-                }
-            }
+            var handheldTVs = Object.FindObjectsByType<HandheldTVCamera>(FindObjectsInactive.Exclude, FindObjectsSortMode.None);
+            foreach (var handheldTV in handheldTVs)
+                handheldTV.DestroyTv();
         }
     }
 }
