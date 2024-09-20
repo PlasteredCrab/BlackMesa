da1a55439c3485ea7ab32b00569bc0e181030ea5
Zaggy1024 <Zaggy1024@gmail.com>
2024-09-19 21:47:10 -0500

    Add a component to spawn existing items in the interior

---
+++ BlackMesa/Components/SpawnVanillaItem.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace BlackMesa.Components;
+
+internal class SpawnVanillaItem : MonoBehaviour
+{
+    public string itemName;
+
+    private void Awake()
+    {
+        var item = GetItemByName(itemName);
+
+        if (item == null)
+            return;
+
+        if (GetComponent<SpawnSyncedObject>() is not { } spawner)
+            spawner = gameObject.AddComponent<SpawnSyncedObject>();
+        spawner.spawnPrefab = item.spawnPrefab;
+    }
+
+    private Item GetItemByName(string name)
+    {
+        foreach (var item in StartOfRound.Instance.allItemsList.itemsList)
+        {
+            if (item.itemName == name)
+                return item;
+        }
+
+        return null;
+    }
+}
