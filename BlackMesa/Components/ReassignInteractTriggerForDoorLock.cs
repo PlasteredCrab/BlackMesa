bb664abba37990e1df7e7593c9674e57abf4ef8b
Zaggy1024 <Zaggy1024@gmail.com>
2024-09-23 14:46:06 -0500

    Add a component to reassign a DoorLock's InteractTrigger
    DoorLock requires an InteractTrigger, but we want to separate it into another object. Add a component to reassign it so we can have a disabled interact trigger on the DoorLock.

---
+++ BlackMesa/Components/ReassignInteractTriggerForDoorLock.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace BlackMesa.Components;
+
+public class ReassignInteractTriggerForDoorLock : MonoBehaviour
+{
+    public DoorLock doorToTrigger;
+    public InteractTrigger interactTrigger;
+
+    private void Start()
+    {
+        doorToTrigger.doorTrigger = interactTrigger;
+    }
+}
