dcec68ee863e62282a178113240ebb39f3c9eba2
Plastered_Crab <49732785+PlasteredCrab@users.noreply.github.com>
2024-09-27 17:27:23 -0700

    Added DamageZone script

--- BlackMesa/DamageZone.cs
+++ BlackMesa/DamageZone.cs
@@ -5,14 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using GameNetcodeStuff;
 using UnityEngine;
-//using Welcome_To_Ooblterra.Properties;
 
 namespace BlackMesa;
-public class AcidWater : MonoBehaviour
+public class DamageZone : MonoBehaviour
 {
 
     public int DamageAmount;
     public float DamageTickRate;
+    public CauseOfDeath cause;
     private float TimeSincePlayerDamaged = 0f;
 
     private void OnTriggerStay(Collider other)
@@ -28,9 +28,9 @@ public class AcidWater : MonoBehaviour
             return;
         }
         if (victim != null)
-        { 
+        {
             TimeSincePlayerDamaged = 0f;
-            victim.DamagePlayer(DamageAmount, hasDamageSFX: true, callRPC: true, CauseOfDeath.Drowning);
+            victim.DamagePlayer(DamageAmount, hasDamageSFX: true, callRPC: true, cause);
             //WTOBase.LogToConsole("New health amount: " + victim.health);
         }
 
