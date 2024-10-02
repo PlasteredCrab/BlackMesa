626a5697a49df8f5ee5dc15f28d4be09f2f1d0d9
Zac <zdhef11@hotmail.com>
2024-10-02 02:55:01 -0400

    Increase MaxAttemptCount for Black Mesa generation to reduce generation failures

---
+++ BlackMesa/Patches/PatchDungeonGenerator.cs
@@ -0,0 +1,32 @@
+﻿using DunGen;
+using HarmonyLib;
+
+namespace BlackMesa.Patches;
+
+[HarmonyPatch(typeof(DungeonGenerator))]
+internal class PatchDungeonGenerator
+{
+    private static int? originalGenerationAttempts;
+
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(DungeonGenerator.Generate))]
+    private static void GenerateNewFloorPrefix(DungeonGenerator __instance)
+    {
+        if (__instance.DungeonFlow != BlackMesaInterior.BlackMesaFlow)
+            return;
+
+        originalGenerationAttempts = __instance.MaxAttemptCount;
+        __instance.MaxAttemptCount *= 5;
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(DungeonGenerator.Generate))]
+    private static void GenerateNewFloorPostfix(DungeonGenerator __instance)
+    {
+        if (!originalGenerationAttempts.HasValue)
+            return;
+
+        __instance.MaxAttemptCount = originalGenerationAttempts.Value;
+        originalGenerationAttempts = null;
+    }
+}
--- BlackMesa/Plugin.cs
+++ BlackMesa/Plugin.cs
@@ -69,6 +69,7 @@ namespace BlackMesa
             harmony.PatchAll(typeof(PatchStartOfRound));
             harmony.PatchAll(typeof(PatchRoundManager));
             harmony.PatchAll(typeof(PatchNetworkManager));
+            harmony.PatchAll(typeof(PatchDungeonGenerator));
             harmony.PatchAll(typeof(PatchLungProp));
 
             const string props = "Assets/LethalCompany/Mods/BlackMesaInterior/DunGen Stuff/Prefabs/Props";
