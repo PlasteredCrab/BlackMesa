e085cada26423b47cf56994fab726a7dacc04825
Zaggy1024 <Zaggy1024@gmail.com>
2024-10-05 00:24:13 -0500

    Add a component to play/pause a particle system with children

---
+++ BlackMesa/Components/PlayParticleAnimationEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace BlackMesa.Components;
+
+internal class PlayParticleAnimationEvent : MonoBehaviour
+{
+    public ParticleSystem particleSystemA;
+
+    public void PlayParticleSystemA()
+    {
+        particleSystemA.Play();
+    }
+
+    public void StopParticleSystemA()
+    {
+        particleSystemA.Stop();
+    }
+}
