304d817ded6830787068fba6afe73c8a53888bae
Zaggy1024 <Zaggy1024@gmail.com>
2024-10-16 16:07:31 -0500

    Add a component to play the blast door effects
    With this, we can specialize the code a little more to make things a bit simpler in the animation clip.

---
+++ BlackMesa/Components/BlastDoorEffects.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace BlackMesa.Components;
+
+public class BlastDoorEffects : MonoBehaviour
+{
+    public AudioSource[] effectAudioSources = [];
+    public ParticleSystem[] effectSparks = [];
+    public AudioSource slamAudioSource;
+    public AudioSource announcementAudioSource;
+
+    public void StartEffects()
+    {
+        foreach (var gearAudioSource in effectAudioSources)
+            gearAudioSource.Play();
+
+        foreach (var sparkEmitter in effectSparks)
+            sparkEmitter.Play();
+    }
+
+    public void EndEffects()
+    {
+        foreach (var gearAudioSource in effectAudioSources)
+            gearAudioSource.Stop();
+
+        foreach (var sparkEmitter in effectSparks)
+            sparkEmitter.Stop();
+    }
+
+    public void PlaySlam()
+    {
+        slamAudioSource.Play();
+
+        if (StartOfRound.Instance.audioListener == null)
+            return;
+        float cameraDistance = Vector3.Distance(StartOfRound.Instance.audioListener.transform.position, slamAudioSource.transform.position);
+        if (cameraDistance < 6)
+            HUDManager.Instance.ShakeCamera(ScreenShakeType.Big);
+        else if (cameraDistance < 12)
+            HUDManager.Instance.ShakeCamera(ScreenShakeType.Small);
+    }
+
+    public void PlayAnnouncement()
+    {
+        announcementAudioSource.Play();
+    }
+}
