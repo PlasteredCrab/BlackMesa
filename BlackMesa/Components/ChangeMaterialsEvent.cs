f485a24c0cd978b69390a65fc86e0c9995294945
Zaggy1024 <Zaggy1024@gmail.com>
2024-10-18 01:02:30 -0500

    Add a component to set all materials on a renderer in an event

---
+++ BlackMesa/Components/ChangeMaterialsEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace BlackMesa.Components;
+
+internal class ChangeMaterialsEvent : MonoBehaviour
+{
+    public Renderer renderer;
+    public Material[] materialsToUse;
+
+    private Material[] originalMaterials;
+
+    private void OnEnable()
+    {
+        originalMaterials = renderer.sharedMaterials;
+        renderer.sharedMaterials = materialsToUse;
+    }
+
+    private void OnDisable()
+    {
+        if (originalMaterials == null)
+            return;
+        renderer.sharedMaterials = originalMaterials;
+        originalMaterials = null;
+    }
+}
