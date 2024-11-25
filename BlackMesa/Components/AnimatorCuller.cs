cbbcd4ef53aec2ea838b02447cf1d352cbab9b99
Zaggy1024 <Zaggy1024@gmail.com>
2024-11-25 12:29:41 -0600

    Use a better method of detecting when to toggle door animators

---
+++ BlackMesa/Components/AnimatorCuller.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackMesa.Components;
+
+public class AnimatorCuller : MonoBehaviour
+{
+    private static readonly Dictionary<Animator, AnimatorCuller> cullers = [];
+
+    private Animator animator;
+
+    private void OnEnable()
+    {
+        animator = GetComponent<Animator>();
+        animator.keepAnimatorStateOnDisable = true;
+        cullers.Add(animator, this);
+    }
+
+    public void CompleteAnimationAndDisableAnimator()
+    {
+        StartCoroutine(DisableAtEndOfFrame(animator));
+    }
+
+    private static IEnumerator DisableAtEndOfFrame(Animator animator)
+    {
+        yield return new WaitForEndOfFrame();
+
+        var currentState = animator.GetCurrentAnimatorStateInfo(0);
+        if (currentState.fullPathHash != 0 && currentState.normalizedTime < 1)
+            yield break;
+        var nextState = animator.GetNextAnimatorStateInfo(0);
+        if (nextState.fullPathHash != 0 && nextState.normalizedTime < 1)
+            yield break;
+
+        animator.enabled = false;
+    }
+
+    internal static void OnAnimationTriggered(Animator animator)
+    {
+        if (!cullers.TryGetValue(animator, out var culler))
+            return;
+        if (culler == null)
+            return;
+        culler.EnableAnimator();
+    }
+
+    public void EnableAnimator()
+    {
+        animator.enabled = true;
+    }
+
+    private void OnDisable()
+    {
+        cullers.Remove(animator);
+    }
+}
---
+++ BlackMesa/Patches/PatchAnimator.cs
@@ -0,0 +1,37 @@
+﻿using BlackMesa.Components;
+using HarmonyLib;
+using UnityEngine;
+
+namespace BlackMesa.Patches;
+
+[HarmonyPatch(typeof(Animator))]
+internal static class PatchAnimator
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Animator.SetTriggerString))]
+    private static void SetTriggerStringPostfix(Animator __instance)
+    {
+        AnimatorCuller.OnAnimationTriggered(__instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Animator.SetTriggerID))]
+    private static void SetTriggerIDPostfix(Animator __instance)
+    {
+        AnimatorCuller.OnAnimationTriggered(__instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Animator.SetBoolString))]
+    private static void SetBoolStringPostfix(Animator __instance)
+    {
+        AnimatorCuller.OnAnimationTriggered(__instance);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(Animator.SetBoolID))]
+    private static void SetBoolIDPostfix(Animator __instance)
+    {
+        AnimatorCuller.OnAnimationTriggered(__instance);
+    }
+}
