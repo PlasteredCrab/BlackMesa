552f8eff7cd9898324dce2b7ec8144fb1e4a8fc6
Zaggy1024 <Zaggy1024@gmail.com>
2024-12-15 17:09:22 -0600

    Add a component to push players along with moving objects

---
+++ BlackMesa/Components/PushRegion.cs
@@ -0,0 +1,51 @@
+﻿using GameNetcodeStuff;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BlackMesa.Components;
+
+public class PushRegion : MonoBehaviour
+{
+    public PushRegion oppositePushRegion;
+
+    private Vector3 lastPosition;
+    private HashSet<PlayerControllerB> playersToPush = [];
+
+    private void Start()
+    {
+        lastPosition = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && other.TryGetComponent(out PlayerControllerB player))
+            playersToPush.Add(player);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && other.TryGetComponent(out PlayerControllerB player))
+            playersToPush.Remove(player);
+    }
+
+    private void LateUpdate()
+    {
+        var position = transform.position;
+        var delta = position - lastPosition;
+
+        if (delta == Vector3.zero)
+            return;
+
+        foreach (var playerToPush in playersToPush)
+        {
+            playerToPush.transform.position += delta;
+            if (oppositePushRegion != null && oppositePushRegion.playersToPush.Contains(playerToPush))
+            {
+                Debug.Log($"{playerToPush} is being pushed by two opposite push regions, killing.");
+                playerToPush.KillPlayer(Vector3.zero, true, CauseOfDeath.Crushing);
+            }
+        }
+
+        lastPosition = position;
+    }
+}
