6f351bf331dca4d66e7656a23d1948a38ef4dd76
Zaggy1024 <Zaggy1024@gmail.com>
2024-12-15 17:10:32 -0600

    Add a component to invoke events when players enter triggers

---
+++ BlackMesa/Components/PlayerTriggerRegion.cs
@@ -0,0 +1,16 @@
+﻿using GameNetcodeStuff;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace BlackMesa.Components;
+
+public class PlayerTriggerRegion : MonoBehaviour
+{
+    public UnityEvent<PlayerControllerB> onStay;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && other.TryGetComponent(out PlayerControllerB player))
+            onStay.Invoke(player);
+    }
+}
